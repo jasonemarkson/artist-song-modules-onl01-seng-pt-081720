2dd635bd99e8ff8df1239aa9374e4d7a6cdf4043
Jason Markson <jasonmarkson76@gmail.com>
2020-09-19 00:30:43 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,6 @@
 module Findable
   def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+    self.detect{|a| a.name == name}
   end
   
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
-
 end
